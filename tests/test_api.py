3be419f61c2630e5eccb9ac773e62d85076c3939
Serdar Tumgoren <zstumgoren@gmail.com>
2021-05-28 19:20:12 -0700

    add test fixture and loosen screws on timestamp testing

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,14 +8,18 @@ from bln_etl.api import Client, Project
 TOKEN=os.environ.get('BLN_API_KEY', 'DUMMY')
 
 
+@pytest.fixture(scope='module')
+def project_uuid():
+    return 'UHJvamVjdDo5ZDQyMzE0NS01OWFhLTQ2N2YtYTZmNy0zMjhiZDI1Yzg3OGI='
+
 @pytest.mark.vcr()
-def test_user_projects():
+def test_user_projects(project_uuid):
     client = Client(TOKEN)
     projects = client.user_projects
     assert len(projects) ==  7
     # Expected attributes
     project = [p for p in projects if p.name == 'Test Project'][0]
-    assert project.id.startswith('UHJvamVjdDo5ZDQyMzE0NS0')
+    assert project.id == project_uuid
     assert project.name == 'Test Project'
     assert project.slug == 'test-project-UHJvamVjdDo5ZDQ'
     assert project.description == 'Just testing out some stuff'
@@ -23,8 +27,8 @@ def test_user_projects():
     assert project.contact == 'tumgoren@stanford.edu'
     assert project.contact_method == 'EMAIL'
     assert project.user_role == 'ADMIN'
-    assert project.created_at == '2021-05-21T00:36:27.510415+00:00'
-    assert project.updated_at == '2021-05-21T16:25:23.827000+00:00'
+    assert project.created_at.startswith('2021')
+    assert project.updated_at.startswith('2021')
 
 
 @pytest.mark.vcr()
